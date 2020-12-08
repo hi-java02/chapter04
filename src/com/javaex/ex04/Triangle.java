1177201c791967bc1f398ca4e0a85e79080a4414
text <text@gmail.com>
2020-12-08 17:50:38 +0900

    ArrayList, LinkedList 사용하기

---
+++ src/com/javaex/ex04/Triangle.java
@@ -0,0 +1,45 @@
+package com.javaex.ex04;
+
+public class Triangle {
+
+	// 필드
+	private int width;
+	private int height;
+
+	// 생성자
+	public Triangle() {
+		}
+
+	public Triangle(int width, int height) {
+			this.width = width;
+			this.height = height;
+		}
+
+	// 메소드 g/s
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	// 메소드 일반
+	public void draw() {
+		System.out.println("삼각형(가로=" + width + ", 세로=" + height + ")을 그렸습니다.");
+	}
+
+	@Override
+	public String toString() {
+		return "Rectangle [width=" + width + ", height=" + height + "]";
+	}
+
+}
